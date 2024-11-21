486e1d4be7b834595d53f8f216e81369167b2f54
Anjelica <54198467+reikamoon@users.noreply.github.com>
2024-11-21 22:48:34 +0000

    updated manifest update script

--- scripts/kargo-argocd-manifestupdate.sh
+++ scripts/kargo-argocd-manifestupdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-for stage in $(kargo get stages --project kargo-simple -o jsonpath='{.items[*].metadata.name}')
+for stage in $(kargo get stages --project kargo-demo -o jsonpath='{.items[*].metadata.name}')
 do
-	kargo get stage ${stage} -p kargo-simple -o json | jq ".spec.promotionTemplate.argoCDAppUpdates[0] += {\"appName\": \"guestbook-simple-${stage}\"}" | kargo apply -f -
+	kargo get stage ${stage} -p kargo-demo -o json | jq ".spec.promotionTemplate.argoCDAppUpdates[0] += {\"appName\": \"guestbook-simple-${stage}\"}" | kargo apply -f -
 done
